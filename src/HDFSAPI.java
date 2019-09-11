79032015eccc0aa3b2f75a4308c2e8a99971cf50
admin <123456>
2019-09-11 11:27:05 +0800

    可以将本地文件上传到hdfs 的data目录

--- src/HDFSAPI.java
+++ src/HDFSAPI.java
@@ -10,12 +10,12 @@ public class HDFSAPI {
         configuration.set("fs.defaultFS","hdfs://127.0.0.1:9000");
         configuration.set("dfs.replication","1");
         FileSystem fileSystem = FileSystem.get(configuration);
-        Path path = new Path("/hdfsdemo2");
+        Path path = new Path("/data");
         if(!fileSystem.isDirectory(path)){
             fileSystem.mkdirs(path);
             System.out.println("创建成功");
         }
-        fileSystem.copyFromLocalFile(new Path("D:\\aa.txt"),new Path("/hdfsdemo2"));
-        fileSystem.copyToLocalFile(new Path("/hdfsdemo2/aa.txt"),new Path("E:\\test\\5.txt"));
+        fileSystem.copyFromLocalFile(new Path("D:\\salaryall.txt"),path);
+//        fileSystem.copyToLocalFile(new Path("/hdfsdemo2/aa.txt"),new Path("E:\\test\\5.txt"));
     }
 }
