6b8d020464cb63b32dfaba902964d20dbb6262fa
admin <123456>
2019-09-11 11:08:03 +0800

    创建项目，第一步写上传初始文件到hdfs

---
+++ src/HDFSAPI.java
@@ -0,0 +1,21 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import java.io.IOException;
+
+public class HDFSAPI {
+    public static void main(String[] args) throws IOException {
+        Configuration configuration = new Configuration();
+        configuration.set("fs.defaultFS","hdfs://127.0.0.1:9000");
+        configuration.set("dfs.replication","1");
+        FileSystem fileSystem = FileSystem.get(configuration);
+        Path path = new Path("/hdfsdemo2");
+        if(!fileSystem.isDirectory(path)){
+            fileSystem.mkdirs(path);
+            System.out.println("创建成功");
+        }
+        fileSystem.copyFromLocalFile(new Path("D:\\aa.txt"),new Path("/hdfsdemo2"));
+        fileSystem.copyToLocalFile(new Path("/hdfsdemo2/aa.txt"),new Path("E:\\test\\5.txt"));
+    }
+}
