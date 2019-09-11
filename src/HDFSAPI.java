155a35ee5c5597c4294b32c3c8555403962945f2
admin <123456>
2019-09-11 15:03:40 +0800

    对数据进行清洗，清洗步骤删除薪资列“面议”，经验列“不限经验”，逗号分隔超过8行，重复列数据清除

--- src/HDFSAPI.java
+++ src/HDFSAPI.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class HDFSAPI {
     public static void main(String[] args) throws IOException {
         Configuration configuration = new Configuration();
-        configuration.set("fs.defaultFS","hdfs://127.0.0.1:9000");
+        configuration.set("fs.defaultFS","hdfs://192.168.43.128:9000");
         configuration.set("dfs.replication","1");
         FileSystem fileSystem = FileSystem.get(configuration);
         Path path = new Path("/data");
@@ -15,7 +15,7 @@ public class HDFSAPI {
             fileSystem.mkdirs(path);
             System.out.println("创建成功");
         }
-        fileSystem.copyFromLocalFile(new Path("D:\\salaryall.txt"),path);
+        fileSystem.copyFromLocalFile(new Path("D:\\test.txt"),path);
 //        fileSystem.copyToLocalFile(new Path("/hdfsdemo2/aa.txt"),new Path("E:\\test\\5.txt"));
     }
 }
